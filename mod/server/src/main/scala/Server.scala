60bb7040e030a486583a77e0d2862e1bed96dd58
Anton Sviridov <keynmol@gmail.com>
2023-08-18 17:12:53 +0100

    Pick up database url from the env

--- mod/server/src/main/scala/Server.scala
+++ mod/server/src/main/scala/Server.scala
@@ -11,10 +11,22 @@ val helloWorldEndpoint = endpoint.get
   .in(query[String]("name"))
   .out(stringBody)
 
+def connection_string() =
+  sys.env.getOrElse(
+    "DATABASE_URL", {
+      val host = sys.env.getOrElse("PG_HOST", "localhost")
+      val port = sys.env.getOrElse("PG_PORT", "5432")
+      val password = sys.env.getOrElse("PG_PASSWORD", "mysecretpassword")
+      val user = sys.env.getOrElse("PG_USER", "postgres")
+      val db = sys.env.getOrElse("PG_DB", "postgres")
+
+      s"postgresql://$user:$password@$host:$port/$db"
+    }
+  )
+
 @main def server =
-  val str = "postgresql://postgres:mysecretpassword@localhost:5432/postgres"
   zone {
-    Db.use(str) { db =>
+    Db.use(connection_string()) { db =>
       val helloWorld =
         helloWorldEndpoint.serverLogic[Id](name =>
           Right(db.getAllRepos.filter(_ == name).toString)
